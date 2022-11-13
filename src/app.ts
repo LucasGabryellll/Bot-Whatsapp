4c60da52f170b4f0e8dee144d201f6c98ff3c866
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-11-13 16:41:26 -0300

    App.js

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express, { Request, Response } from "express";
 import * as bodyParser from "body-parser";
 import routes from "./routes";
 
+import { threadNewMessage } from './Controller/ZabbixTriggerController';
+
+setInterval(() => threadNewMessage(), 40000);
+
 const app = express();
 app.use(bodyParser.json());
 app.use(routes);
 
 app.listen(5000, () => {
-    console.log("Server  is running in: http://localhost:5000");
+  console.log("Server  is running in: http://localhost:5000");
 })
